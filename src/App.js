34ff089a61ec173c482c1d70308ad1e62b4c8ad2
Ibrahim <hemahema684@gmail.com>
2024-09-28 18:57:21 +0200

    edit app.js file

---
+++ src/App.js
@@ -0,0 +1,19 @@
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+
+<section>
+  <h1 className="h1dev">ibrahim</h1>
+  <h2>darknes442</h2>
+</section>
+
+      </header>
+    </div>
+  );
+}
+
+export default App;
